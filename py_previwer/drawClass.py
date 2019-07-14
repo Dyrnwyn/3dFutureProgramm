23b96754ec66df2727ced312599d2de8e0cb5661
Roman Gorovoi <pod1987@gmail.com>
2019-07-15 00:20:29 +0700

    Немного изменений

--- py_previwer/drawClass.py
+++ py_previwer/drawClass.py
@@ -13,7 +13,7 @@ class drawPage(object):
     def changeFont(self, fontName="Font/a_Futurica.ttf", fontSize=40):
         self.font = ImageFont.truetype(fontName, fontSize)
 
-    def drawLinePage(self):  # Метод рисования линий на листе
+    def drawLinePage(self):  # Метод разметки страницы на ячейки
         # -------------horizontal line--------------------
         self.draw.line([(60, 400), (2420, 400)], fill=0, width=7)
         self.draw.line([(60, 1924), (2420, 1924)], fill=0, width=7)
@@ -25,7 +25,7 @@ class drawPage(object):
         self.draw.text((60, 40), "Class")
 
     def drawTitle(self):
-        #  Метод в котором пишем в заголовке
+        #  Метод в котором выводим в заголовке
         # "класс/группа" и "Наименование объекта"
         self.draw.text((60, 30), "класс/группа", font=self.font, fill=0)
         fontWidth = self.font.getsize("Наименование Объекта")[0]
@@ -33,6 +33,7 @@ class drawPage(object):
                        "Наименование Объекта", font=self.font, fill=0)
 
     def drawNameObject(self, nameObject="Без имени"):
+        # В методе выводим название объекта
         fontWidth = self.font.getsize(nameObject)[0]
         self.draw.text((self.page.width / 2 - fontWidth / 2, 100),
                        nameObject, font=self.font, fill=0)
@@ -42,10 +43,12 @@ class drawPage(object):
         self.draw.text((60, 100), nameKlass, font=self.font, fill=0)
 
     def drawProductParametrs(self, numbCell=1):
+        # метод вывода информации об изделии
+        # в ячейки
         dictXY = {1: (70, 1500),
                   2: (1220, 1500),
-                  3: (1220, 3050),
-                  4: (70, 3050)
+                  3: (70, 3050),
+                  4: (1220, 3050)
                   }
         parametrs = ("Вид: плоска\n" +
                      "Размер: 10х15\n" +
@@ -55,17 +58,33 @@ class drawPage(object):
                      )
         self.draw.text(dictXY[numbCell], parametrs, font=self.font, fill=0)
 
+    def addImg(self, imgPath, cell):
+        # метод вывода фото изделия в ячейки 
+        dictXY = {1: (120, 500),
+                  2: (1270, 500),
+                  3: (120, 2100),
+                  4: (1270, 2100)
+                  }
+        img = Image.open(imgPath)
+        self.page.paste(img, dictXY[cell])
+
     def savePage(self):
-        self.page.save("1.jpg")
+        try:
+            self.page.save("1.pdf", append=True, resolution=300)
+        except FileNotFoundError:
+            self.page.save("1.pdf", resolution=300)
+
 
 
 if __name__ == "__main__":
-    page1 = drawPage()
-    page1.drawLinePage()
-    page1.drawTitle()
-    page1.changeFont(fontSize=100)
-    page1.drawNameObject("Новосибирск 111221")
-    page1.drawOfKlass("7б класс")
-    page1.changeFont(fontSize=60)
-    page1.drawProductParametrs(3)
-    page1.savePage()
+    for i in range(10):
+        page1 = drawPage()
+        page1.drawLinePage()
+        page1.drawTitle()
+        page1.changeFont(fontSize=100)
+        page1.drawNameObject("Новосибирск 111221")
+        page1.drawOfKlass("7б класс")
+        page1.changeFont(fontSize=60)
+        page1.drawProductParametrs(4)
+        page1.addImg("/media/work_part/python/Ижевск 40 ш 1в досьемка/tmp/о_магнит 10х15_4030_7802_1_1в_H.jpeg", 4)
+        page1.savePage()
--- py_previwer/previwer.py
+++ py_previwer/previwer.py
@@ -3,15 +3,17 @@ from psd_tools2 import PSDImage
 import re
 from PIL import Image
 
+tmpDir = 'tmp'
 
-def searchPsd():
-    psdList = []
-    with os.scandir() as flList:
+
+def searchFl(flExt, fldr=""):
+    filteredFlList = []
+    with os.scandir(fldr) as flList:
         for fl in flList:
             if not fl.name.startswith('.') and fl.is_file() and \
-            re.findall(r"^.*\.psd*", fl.name):
-                psdList.append(fl.name)
-    return psdList
+                    re.findall(r"^.*\." + flExt + "*", fl.name):
+                filteredFlList.append(fl.name)
+    return filteredFlList
 
 
 def convertPsd(psdFl, tmpDir):
@@ -26,28 +28,37 @@ def convertPsd(psdFl, tmpDir):
     os.remove(tmpDir + "/" + "tmp_file.png")
 
 
-def createTmpDir(tmpFolder):
+def createTmpDir():
+    # Создаем временную директорию
     try:
-        os.mkdir(tmpFolder)
-    except Exception as e:
-        pass    
-    return True
+        os.mkdir(tmpDir)
+    except FileExistsError:
+        pass
 
 
-def removeTmpDir(tmpDir):
-    for root, dirs, files in os.walk(folder + tmpFolder):
-        for fl in files:
-            os.remove(folder + tmpFolder + '/' + fl)
-        for dr in dirs:
-            os.removedirs(dirs)
+def removeTmpDir():
+    folders = []
+    for i in os.walk(tmpDir):
+        folders.append(i[0])
+        for fl in i[2]:
+            os.remove(i[0] + '/' + fl)
+    folders.reverse()
+    for i in folders:
+        os.removedirs(i)
+
+
+def createDictClass(flList):
+    
 
 
 def pyMain(folder='/media/work_part/python/Ижевск 40 ш 1в досьемка/'):
     os.chdir(folder)
-    tmpDir = 'tmp'
-    createTmpDir(tmpDir)
-    psdFl = searchPsd()
-    convertPsd(psdFl, tmpDir)
+    #createTmpDir()
+    #psdFl = searchFl("psd")
+    #convertPsd(psdFl, tmpDir)
+    jpgFl = searchFl("jpeg", "tmp")
+    createDictClass(jpgFl)
+    # removeTmpDir()
 
 
 if __name__ == "__main__":
