7041fa3058bfb07174de6f7bf4b292cf538d1621
Roman Gorovoi <pod1987@gmail.com>
2019-09-06 22:00:33 +0700

    change gui

--- py_previwer/gui.py
+++ py_previwer/gui.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QFileDialog,
-                             QLineEdit, QGridLayout, QProgressBar, QMessageBox)
+                             QLineEdit, QGridLayout, QProgressBar, QMessageBox,
+                             QGroupBox)
 import previewer
 import os
 
@@ -14,10 +15,13 @@ class guiInterface(QWidget):
         self.setGeometry(0, 0, 400, 100)
         self.setWindowTitle("Превьювер")
         self.move(650, 490)
+        self.grpboxPath = QGroupBox("Выберите объект", self)
+        self.grpboxObjectName = QGroupBox("Выберите объект", self)
         self.btn = QPushButton('Старт', self)
         self.rbtn = QPushButton('Обзор', self)
         self.qbtn = QPushButton('Выход', self)
         self.le = QLineEdit(self)
+        self.leObjectName = QLineEdit(self)
         self.prgBar = QProgressBar(self)
         self.prgBar.setMaximum(100)
         self.rbtn.clicked.connect(self.getPath)
@@ -26,15 +30,28 @@ class guiInterface(QWidget):
         self.msgBox = QMessageBox(self)
         self.msgBox.setText("Готово")
 
+        gridPath = QGridLayout()
+        gridPath.addWidget(self.le, 1, 0)
+        gridPath.addWidget(self.rbtn, 1, 1)
+        self.grpboxPath.setLayout(gridPath)
+
+        gridObjectName = QGridLayout()
+        gridObjectName.addWidget(self.leObjectName, 2, 0)
+        self.grpboxObjectName.setLayout(gridObjectName)
+
         grid = QGridLayout()
         grid.setSpacing(10)
         grid.addWidget(self.prgBar, 1, 0, 1, 2)
-        grid.addWidget(self.le, 2, 0)
-        grid.addWidget(self.rbtn, 2, 1)
-        grid.addWidget(self.btn, 3, 0)
-        grid.addWidget(self.qbtn, 3, 1)
+        grid.addWidget(self.grpboxPath, 2, 0)
+        grid.addWidget(self.grpboxObjectName, 3, 0)
+        grid.addWidget(self.btn, 6, 0)
+        grid.addWidget(self.qbtn, 6, 1)
+
         self.setLayout(grid)
-        self.setWindowTitle('Превьювер')
+
+
+
+
         self.show()
 
     def getPath(self):
